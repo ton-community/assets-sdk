034451fd9dba6f12a872999b28ae66be3765cd64
Tikhon Slasten <slasten32@gmail.com>
2023-10-02 22:23:13 +0800

    Add AWS S3 storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,5 +1,7 @@
 import PinataClient from '@pinata/sdk';
 import { Readable } from 'stream';
+import { S3 } from "@aws-sdk/client-s3"
+import { AwsCredentialIdentity } from "@aws-sdk/types";
 
 export interface Storage {
     uploadFile(contents: Buffer): Promise<string>;
@@ -18,5 +20,32 @@ export class PinataStorage implements Storage {
                 name: 'GameFi SDK Jetton',
             }
         })).IpfsHash;
+    }
+}
+
+export class S3Storage implements Storage {
+    private readonly bucket: string;
+    private readonly s3: S3;
+
+    constructor(accessKeyId: string, secretAccessKey: string, bucket: string) {
+        this.bucket = bucket;
+        this.s3 = new S3({
+            credentials: {
+                accessKeyId: accessKeyId!,
+                secretAccessKey: secretAccessKey!,
+            } as AwsCredentialIdentity,
+        });
+    }
+
+    async uploadFile(contents: Buffer): Promise<string> {
+        const key = 'jetton/' + Math.random().toString(36).substring(2)
+
+        await this.s3.putObject({
+            Bucket: this.bucket,
+            Key: key,
+            Body: contents,
+        })
+
+        return 'https://' + this.bucket + '.s3.amazonaws.com/' + key;
     }
 }
