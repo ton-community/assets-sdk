e3decab2817020a91f396e1fc8635dcd3e2dc04f
MK <mahdikarami1000@gmail.com>
2023-10-02 21:12:53 +0330

    add example for mint more add example open jetton add example transfer jetton

---
+++ examples/access-to-current-jetton.ts
@@ -0,0 +1,21 @@
+import { Address, toNano } from "@ton/core";
+import { GameFiSDK, createWalletV4 } from "../src/index";
+
+async function main() {
+    const sdk = await GameFiSDK.create({
+        storage: {
+            pinataApiKey: process.env.PINATA_API!,
+            pinataSecretKey: process.env.PINATA_SECRET!,
+        },
+        api: 'testnet',
+        wallet: await createWalletV4(process.env.MNEMONIC!),
+    });
+
+    console.log('Using wallet', sdk.sender?.address);
+
+    const jetton = await sdk.openJetton(Address.parse("my-jetton-address"));
+
+    console.log('Loaded jetton with address', jetton.address);
+}
+
+main();
---
+++ examples/mint-more.ts
@@ -0,0 +1,24 @@
+import { Address, toNano } from "@ton/core";
+import { GameFiSDK, createWalletV4 } from "../src/index";
+
+async function main() {
+    const sdk = await GameFiSDK.create({
+        storage: {
+            pinataApiKey: process.env.PINATA_API!,
+            pinataSecretKey: process.env.PINATA_SECRET!,
+        },
+        api: 'testnet',
+        wallet: await createWalletV4(process.env.MNEMONIC!),
+    });
+
+    console.log('Using wallet', sdk.sender?.address);
+
+    const jetton = await sdk.openJetton(Address.parse("my-jetton-address"));
+
+    await jetton.sendMint({
+        to: Address.parse("any-address"),
+        amount: 1200000n
+    })
+}
+
+main();
