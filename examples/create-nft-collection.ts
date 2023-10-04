1343c3b1030e5b0ca86ed7bd099354627a24b6e8
MK <mahdikarami1000@gmail.com>
2023-10-04 11:15:08 +0330

    add example for nft parts

---
+++ examples/create-nft-collection.ts
@@ -0,0 +1,37 @@
+import { Address, toNano } from "@ton/core";
+import { GameFiSDK, createWalletV4 } from "../src/index";
+
+async function main() {
+    const sdk = await GameFiSDK.create({
+        storage: {
+            pinataApiKey: process.env.PINATA_API!,
+            pinataSecretKey: process.env.PINATA_SECRET!,
+        },
+        api: 'testnet',
+        wallet: await createWalletV4(process.env.MNEMONIC!),
+    });
+
+    console.log('Using wallet', sdk.sender?.address);
+
+    const nftCollection = await sdk.createNftCollection({
+        collectionContent: {
+            uri: "test-uri",
+            name: "test-name",
+            description: "lorem ipsum",
+            image: "test-image",
+        },
+        commonContent: "test-content"
+    }, {
+        premint: {
+            itemIndex: 1n,
+            itemParams: {
+                individualContent: "test",
+                owner: Address.parse("test-address")
+            }
+        },
+    });
+
+    console.log('Created nft collection with address', nftCollection.address);
+}
+
+main();
---
+++ examples/transfer-nft-item.ts
@@ -0,0 +1,28 @@
+import { Address, toNano } from "@ton/core";
+import { GameFiSDK, createWalletV4 } from "../src/index";
+
+async function main() {
+    const sdk = await GameFiSDK.create({
+        storage: {
+            pinataApiKey: process.env.PINATA_API!,
+            pinataSecretKey: process.env.PINATA_SECRET!,
+        },
+        api: 'testnet',
+        wallet: await createWalletV4(process.env.MNEMONIC!),
+    });
+
+    console.log('Using wallet', sdk.sender?.address);
+
+
+    const nftCollection = await sdk.openNftCollection(Address.parse("my-nft-collection-address"));
+    nftCollection.getData().
+    nftCollection.sendMint({
+        itemIndex: 2n,
+        itemParams: {
+            individualContent: "test-2",
+            owner: Address.parse("test-address-2")
+        }
+    })
+}
+
+main();
